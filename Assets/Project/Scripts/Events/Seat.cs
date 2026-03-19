e2330c6a9760a6dd3a0ffa61b4f156749ec5e0a3
Ani-Utkin <annau25@gmail.com>
2026-03-19 15:42:12 -0400

    Seat Event

--- Assets/Project/Scripts/Events/Seat.cs
+++ Assets/Project/Scripts/Events/Seat.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class Seat : MonoBehaviour, IEvent 
 {
+    
+    [SerializeField] GameObject seat;
+    public bool hasAppeared;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        seat.SetActive(false);
     }
 
     // Update is called once per frame
@@ -16,7 +20,9 @@ public class Seat : MonoBehaviour, IEvent
 
     public void Appear()
     {
-        
+        hasAppeared = true;
+        seat.SetActive(true);
+        SanityManager.Instance.DecreaseSanity(5);
     }
 
 }
