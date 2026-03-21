016e6361d1b643cd29fe78a7c44e8cc7d8df9536
Ani-Utkin <annau25@gmail.com>
2026-03-21 11:54:24 -0400

    Added timer

---
+++ Assets/Project/Scripts/Player/TimerController.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TimerController : MonoBehaviour
+{
+    public static TimerController Instance { get; set; }
+    public UnityEvent<float> TimerUpdated = new();
+
+    public void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        } 
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    void Update()
+    {
+        StartTimer();
+    }
+
+
+    public void StartTimer()
+    {
+        TimerUpdated.Invoke(0);
+    }
+
+
+}
--- Assets/Project/UI/HUD/HUDController.cs
+++ Assets/Project/UI/HUD/HUDController.cs
@@ -5,6 +5,7 @@ public class HUDController : MonoBehaviour
 {
     UIDocument _hudDocument;
     VisualElement _sanityBarFill;
+    Label _timer;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,11 +14,18 @@ public class HUDController : MonoBehaviour
 
         _sanityBarFill = _hudDocument.rootVisualElement.Q<VisualElement>("sanity-bar-fill");
         _sanityBarFill.style.backgroundColor = Color.green;
+        
+        _timer = _hudDocument.rootVisualElement.Q<Label>("timer");
 
         if (SanityManager.Instance)
         {
             Debug.Log("Sanity Manager Exists");
             SanityManager.Instance.SanityUpdated.AddListener(SetSanityBarFillSize);
+        }
+
+        if (TimerController.Instance)
+        {
+            Debug.Log("Timer Exists");
         }
     }
 
@@ -29,5 +37,13 @@ public class HUDController : MonoBehaviour
         else  _sanityBarFill.style.backgroundColor = Color.red;
 
         _sanityBarFill.style.width = widthLength;
+    }
+
+    public void SetTimer(float time)
+    {
+        time += Time.deltaTime;
+        int minutes = Mathf.FloorToInt(time / 60);
+        int seconds = Mathf.FloorToInt(time % 60);
+        _timer.text = $"{minutes:00}:{seconds:00}";
     }
 }
